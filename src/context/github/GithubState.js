03228c943c3f917bc1d56d7d20e59abb9fa14bc5
Jaysone <kennethjaysone@gmail.com>
2020-03-29 23:06:06 +0800

    Update GithubState.js
    Cleaned up the secrets for github app for prep to deploy to netlify

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -13,6 +13,17 @@ import {
   GET_REPOS
 } from "../types";
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 // Set state
 const GithubState = props => {
   const initialState = {
@@ -34,7 +45,7 @@ const GithubState = props => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -47,7 +58,7 @@ const GithubState = props => {
   const getUser = async login => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${login}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${login}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({
       type: GET_USER,
@@ -60,7 +71,7 @@ const GithubState = props => {
   const getUserRepos = async username => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
